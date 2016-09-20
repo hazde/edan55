1bbcd790aa44a4bf27904a9222fa8f961cfb5617
Hans Bjerndell <dat12hbj@venus-4.student.lth.se>
2016-09-20 12:21:31 +0200

    Changed some things

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -14,31 +14,41 @@ v_count = info[0]
 
 
 def flip(p):
-    return 0 if rnd.random() < p else 1
+    return 0 if rnd.uniform(0.0, 1.0) < p else 1
 
 
 def run():
+    totsum = 0
     for t in lines[1:]:
         vals = re.split(r'\s', t)
-        if len(vals) > 1: edges[tuple(vals[:2])] = vals[2]
-
-    cut = list()
+        
+        if len(vals) > 1:
+            edges[tuple(vals[:2])] = vals[2]
+            totsum += int(vals[2])
+    
+    print(totsum)
+    print(totsum/2)
+    
+    cut = dict()
     for i in range(int(v_count)):
-        if flip(0.5) == 1:
-            cut.append(str(i))
+        if flip(0.5):
+            cut[str(i)] = i
 
     maxcut = 0
-
     for t in edges:
         if (t[0] in cut and t[1] not in cut) or (t[0] not in cut and t[1] in cut):
             maxcut += int(edges[(t[0], t[1])])
     return maxcut
 
+run()
+"""
 tot = 0
-for i in range(0, 100):
+iterations = 100
+for i in range(0, iterations):
     thisrun = run()
     print(thisrun)
     tot += thisrun
-tot /= 100
-print(tot)
+tot /= iterations
+"""
+#print(tot)
 
